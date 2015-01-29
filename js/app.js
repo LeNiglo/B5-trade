ba311c9ff81120e34fcaa32164645b6491d96ad3
LeNiglo <lefrantguillaume@gmail.com>
2015-01-29 21:25:37 +0100

    up

--- js/app.js
+++ js/app.js
@@ -2,4 +2,24 @@
 
 	var socket = io.connect('http://localhost:1337');
 
+	var titles = [];
+	var cours = [];
+	socket.on('init', function(data) {
+		titles = data.titles;
+		cours = data.cours;
+		var str = '<td></td>'
+		cours.forEach(function (elem, index) {
+			str += '<th scope="col">'+index+'</th>';
+		});
+		$('#trade').find('thead').append(str);
+		titles.forEach(function (elem, index) {
+			str = '<th scope="row">'+elem+'</th>';
+			cours.forEach(function (elem) {
+				str += '<td>'+elem[index]+'</td>';
+			});
+			$('#trade').find('tbody').append(str);
+		});
+		chart = $('#trade').visualize({type: 'area', height: 500});
+	});
+
 })(jQuery);
--- server.js
+++ server.js
@@ -8,15 +8,44 @@ var rl = require('readline').createInterface({
 	output: process.stdout
 });
 
+
+/*
+** Here are to usefull variables
+*/
+
+var title = [];
+var i = 0;
+var cours = [];
+
 /* Do something on client connection */
 io.on('connection', function() {
-	
+	console.log("New User : Broadcasting !");
+	io.sockets.emit('init', {titles: title, cours: cours});
 });
 
 server.listen(1337, function() {
 	/* Start the process here */
+	process_throw_file();
+
+});
+
+process_throw_file = function() {
 
 	rl.on('line', function (cmd) {
-		console.log('You just typed: '+cmd);
+		var line = cmd.split(';');
+		if (i == 0) {
+			title = line;
+			console.log('title', title);
+		} else {
+			var obj = {	};
+
+			line.forEach(function (elem, index, array) {
+				obj[index] = elem;
+				console.log("["+i+"] {"+title[index]+"} "+elem);
+			});
+			cours.push(obj);
+		}
+		++i;
 	});
-});
+
+}
